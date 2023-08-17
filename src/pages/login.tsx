de2b98e66d8441bc3c5282deb265badf6298951d
Streltsov Ivan <principdereva@gmail.com>
2023-08-17 22:22:44 +0200

    added login logic

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,39 @@
+import { useNavigate } from 'react-router-dom';
 import Logo from '../components/logo';
-import { useAppSelector } from '../redux-hooks';
+import { useAppDispatch, useAppSelector } from '../redux-hooks';
+import { useRef } from 'react';
+
+
+type AuthData = {
+  email: string;
+  password: string;
+}
 
 function Login(): JSX.Element {
-  const currentCity = useAppSelector((state)=> state.city);
+  const currentCity: string = useAppSelector((state)=> state.city);
+
+
+  const emailRef = userRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (authData: AuthData)=> {
+    dispatch(loginAction(authData));
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.prevent.default();
+
+    if (emailRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+    }
+  };
 
   return(
     <div className="page page--gray page--login">
@@ -22,13 +53,30 @@ function Login(): JSX.Element {
             <form className="login__form form" action="#" method="post">
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  id="email"
+                  ref={emailRef}
+                  placeholder="Email"
+                  required />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input
+                  className="login__input form__input"
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  ref={passwordRef}
+                  required />
               </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
+              <button
+                className="login__submit form__submit button"
+                type="submit"
+                onClick={handleSubmit}>Sign in</button>
             </form>
           </section>
           <section className="locations locations--login locations--current">

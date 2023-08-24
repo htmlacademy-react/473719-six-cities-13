51622c250d0ab9971c45c7d3a174319ab8c195c7
Streltsov Ivan <principdereva@gmail.com>
2023-08-24 21:09:26 +0200

    fixed typisation

--- src/components/comment-section/comment-section.tsx
+++ src/components/comment-section/comment-section.tsx
@@ -29,10 +29,19 @@ function CommentSection():JSX.Element {
           <StarMemo key={rate} rate={rate} chooseStar ={chooseStar} isSending={isSending}></StarMemo>
         )}
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={review.comment} onChange={changeText} disabled={isSending}></textarea>
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={review.comment}
+        onChange={changeText}
+        disabled={isSending}
+      >
+      </textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-      To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
         <button
           className="reviews__submit form__submit button"
--- src/custom-hooks/use-filtering.tsx
+++ src/custom-hooks/use-filtering.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useAppDispatch } from '../redux-hooks';
 import { choseFilter } from '../store/actions';
 
-function useFiltering () {
+function useFiltering (): [string, boolean, ((event: React.MouseEvent<HTMLElement, MouseEvent>) => void), ((event: React.MouseEvent<HTMLElement, MouseEvent>) => void)] {
   const dispatch = useAppDispatch();
   const [filter, setFilter] = useState('Popular');
   const [isOpened, setOpened] = useState(false);

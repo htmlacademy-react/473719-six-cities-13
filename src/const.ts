6cb8aa78d4c506c1da5f3156b26349cbce097706
Streltsov Ivan <principdereva@gmail.com>
2023-08-27 23:29:54 +0200

    rid off AllCities cons

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,3 @@
-import { City } from './types/types';
 import { StatusCodes } from 'http-status-codes';
 
 export const EMPTY_LINE = '';
@@ -59,47 +58,6 @@ export const CITIES: GenericObject = {
   hamburg: 'Hamburg',
   dusseldorf: 'Dusseldorf'
 } as const;
-
-export const AllCities: Array<City> = [
-  {
-    'name': 'Paris',
-    'location': {
-      'latitude': 48.85661,
-      'longitude': 2.351499,
-      'zoom': 13
-    }},
-  {
-    'name': 'Cologne',
-    'location': {
-      'latitude': 50.938361,
-      'longitude': 6.959974,
-      'zoom': 13
-    }},
-  {
-    'name':'Amsterdam',
-    'location':{
-      'latitude':52.37454,
-      'longitude':4.897976,
-      'zoom':13}},
-  {
-    'name':'Hamburg',
-    'location':{
-      'latitude':53.550341,
-      'longitude':10.000654,
-      'zoom':13}},
-  {
-    'name':'Dusseldorf',
-    'location':{
-      'latitude':51.225402,
-      'longitude':6.776314,
-      'zoom':13}},
-  {
-    'name':'Brussels',
-    'location':{
-      'latitude':50.846557,
-      'longitude':4.351697,
-      'zoom':13}},
-];
 
 export const enum APIroute {
   Offers = '/offers',

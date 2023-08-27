24560352b0d6304bd0a2599f1b27ef0eb2071e67
Streltsov Ivan <principdereva@gmail.com>
2023-08-27 20:23:29 +0200

    added render of quantity of comments

--- src/components/reviews-list/reviews-list.tsx
+++ src/components/reviews-list/reviews-list.tsx
@@ -9,7 +9,7 @@ function ReviewsList(): JSX.Element {
   const loadedComments = useAppSelector(getComments);
   return(
     <React.Fragment>
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{loadedComments.length}</span></h2>
       <ul className="reviews__list">
         {loadedComments.map((item: ReviewItemProps) => <ReviewItem key={item.id} {...item}/>)}
       </ul>

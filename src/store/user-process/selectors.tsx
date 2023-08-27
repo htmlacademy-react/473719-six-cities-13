351b4aab8c268c416afb78ced523f5c5b77afdf8
Streltsov Ivan <principdereva@gmail.com>
2023-08-27 22:46:11 +0200

    Fixed render of email in Login

--- src/store/user-process/selectors.tsx
+++ src/store/user-process/selectors.tsx
@@ -1,7 +1,7 @@
 import { NameSpace } from '../../const';
 import { State } from '../../types/state';
 import { AuthorizationStatus } from '../../const';
-import { UserData } from '../../types/types';
+import { UserData } from '../../types/auth-user-data';
 
 export const getAuthorizationStatus = (state: State): AuthorizationStatus => state[NameSpace.User].authorizationStatus;
 export const getAuthCheckedStatus = (state: State): boolean => state[NameSpace.User].authorizationStatus !== AuthorizationStatus.Unknown;
